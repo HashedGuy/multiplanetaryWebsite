bfac0cfa3a3d012f01ec90bbac89b7e346262c04
HashedGuy <hashedguy@gmail.com>
2021-10-23 20:27:28 +0200

    edited heropis content(2)

--- src/components/PeopleInSpace/HeroPIS.js
+++ src/components/PeopleInSpace/HeroPIS.js
@@ -21,7 +21,7 @@ function HeroPhotos() {
 
             <div className="hero-body">
                 <h2 className="hero-title">{astNumber} people in space right now</h2>
-                <p className="hero-info">Some of us has already left the Earth...Thanks to Open Notify and the WTIA REST API, we can get the updated list of the astronauts in space, as well as the current position of their satellite, timezone information and many more./p>
+                <p className="hero-info">Some of us has already left the Earth...Thanks to Open Notify and the WTIA REST API, we can get the updated list of the astronauts in space, as well as the current position of their satellite, timezone information and many more.</p>
             </div>
         </div>
     )
