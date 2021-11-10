cbc3c44a1ed9461ee3592d19bc2bca991fb216cc
HashedGuy <hashedguy@gmail.com>
2021-11-10 19:47:02 +0100

    adding TSS

--- src/components/PeopleInSpace/Astronauts.js
+++ src/components/PeopleInSpace/Astronauts.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import './pis.css'
 import ISSImg from "../../img/iss_croped.png"
-import ShenzhouImg from "../../img/shenzhou.png"
+import TongImg from "../../img/tiangongX.png"
 import SoyuzImg from "../../img/soyuz.png"
 import DragonImg from "../../img/dragon.png"
 
@@ -144,7 +144,7 @@ function Astronauts() {
                     <div className="craft-details">
                         <div className = "craft-visual">
                             <img 
-                                src={craft === 'Shenzhou 13' ? ShenzhouImg : ISSImg} 
+                                src={craft === 'Shenzhou 13' ? TongImg : ISSImg} 
                                 className={craft === 'Shenzhou 13' ? "add-padding" : ""}
                             />
 
@@ -191,10 +191,10 @@ function Astronauts() {
                         }
                         </div>
                         <div className="craft-info">
-                            <h1>{craft === 'Shenzhou 13' ? "Shenzhou 13": "International Space Station (ISS)"}</h1>
+                            <h1>{craft === 'Shenzhou 13' ? "Tiangong Space Station (TSS)": "International Space Station (ISS)"}</h1>
                             <p>{craft === 'Shenzhou 13' ? 
                                 
-                                "Shenzhou 13 (Chinese: 神舟十三号; pinyin: Shénzhōu Shísān-hào) is the eighth crewed Chinese spaceflight and the thirteenth flight of the Shenzhou program. The spacecraft carried three People's Liberation Army Astronaut Corps (PLAAC) taikonauts on the second flight to the Tianhe core module, the first module of the Tiangong space station. The launch of the three-person crew with a Long March-2F launch vehicle from the Jiuquan Satellite Launch Center took place on October 15, 2021 at 16:23 UTC, and the return is planned for spring 2022. "
+                                "Tiangong (Chinese: 天宫; pinyin: Tiāngōng; lit. 'Heavenly Palace'), officially the Tiangong space station (Chinese: 天宫空间站), is a space station being constructed by China in low Earth orbit between 340 and 450 km (210 and 280 mi) above the surface. Being China's first long-term space station, it is the goal of the 'Third Step' of the China Manned Space Program. Once completed, the Tiangong Space Station will have a mass between 80 and 100 t (180,000 and 220,000 lb), roughly one-fifth the mass of the International Space Station and about the size of the decommissioned Russian Mir space station."
                                 :
                                 "The International Space Station (ISS) is a modular space station (habitable artificial satellite) in low Earth orbit. It is a multinational collaborative project involving five participating space agencies: NASA (United States), Roscosmos (Russia), JAXA (Japan), ESA (Europe), and CSA (Canada). The station serves as a microgravity and space environment research laboratory in which scientific research is conducted in astrobiology, astronomy, meteorology, physics, and other fields."}</p>
                         </div>
