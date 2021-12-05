580a0f0058ca95b191be4613edb5e7890f852154
HashedGuy <hashedguy@gmail.com>
2021-12-05 21:48:14 +0400

    add

--- src/components/BehindScenes/BehindScenes.js
+++ src/components/BehindScenes/BehindScenes.js
@@ -5,12 +5,12 @@ function BehindScenes() {
     return (
         <div className="aloha">
             <h2>Houston, we gotta problem...</h2>
-            <p>This is Arbus, a self-taught astronaut (just kidding). A space enthusiaist who would like to help the community to visualize all the vibe around space in this website with the help of humble animations. I just started to build this project (a couple of months ago) and I am excited to take it to the next level and make more publicly available data for the space-enthusiaists.</p>
+            <p>This is Arbus, a self-taught astronaut (just kidding). A space enthusiaist who enjoys to visualize the vibe around space with the help of humble animations. I just started to build this project (a couple of months ago) and I am excited to take it to the next level and make more publicly available data for the community.</p>
 
             <br />
-            <p>Would love to hear your comments/critique or any offers if you have any. Feel free to hit me up at <a href="mailto:arbus@multiplanetary.website">arbus@multiplanetary.website</a></p>
+            <p>Would love to hear your comments/critique/offers if you have any. Feel free to hit me up at <a href="mailto:arbus@multiplanetary.website">arbus@multiplanetary.website</a></p>
             <br />
-            <p>Some technical details: the website is built on React, nothing fancy simple HTML, CSS (even the animations are CSS-only). I offer freelance web-dev services. Feel free to contact if you're interested in.</p>
+            <p className="yavash">Some technical details: the website is built on React, nothing fancy simple HTML, CSS (yep, the animations are CSS-only too). I offer freelance web-dev services as well. Feel free to contact if you're interested in.</p>
             <a className="btn">Wanna support? You kidding</a>
         </div>
     )
