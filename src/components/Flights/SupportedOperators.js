87533fc31eb9f52238774d6c3810ba13d0040995
HashedGuy <hashedguy@gmail.com>
2021-12-10 20:07:42 +0400

    add

--- src/components/Flights/SupportedOperators.js
+++ src/components/Flights/SupportedOperators.js
@@ -5,14 +5,15 @@ function SupportedOperators() {
         <div className="supported-operators">
             <h1>Available operators</h1>
             <div className="operators">
-                <a><img src={`logos/spacex.png`} className="supported-operators-logo" title="SpaceX" alt="spacex"/></a>
-                <a><img src={`logos/arianespace.png`} className="supported-operators-logo" title="ArianeSpace" alt="ariane-space"/></a>
-                <a><img src={`logos/rocket-lab.png`} className="supported-operators-logo" title="Rocket Lab" alt="rocket-lab"/></a>
-                <a><img src={`logos/blue-origin.png`} className="supported-operators-logo" title="Blue Origin" alt="blue-origin"/></a>
-                <a><img src={`logos/astra-space.png`} className="supported-operators-logo" title="Astra Space" alt="astra-space"/></a>
-                <a><img src={`logos/roscosmos.png`} className="supported-operators-logo" title="Roscosmos" alt="roscosmos"/></a>
-                <a><img src={`logos/ula-white.png`} className="supported-operators-logo" title="United Launch Alliance" alt="united-launch-alliance"/></a>
-                <a><img src={`logos/virgin-orbit.png`} className="supported-operators-logo soon" title="Virgin Orbit (coming soon)" alt="virgin-orbit"/></a>
+                <a><img src={`logos/spacex.png`} className="supported-operators-logo" title="SpaceX"/></a>
+                <a><img src={`logos/arianespace.png`} className="supported-operators-logo" title="ArianeSpace"/></a>
+                <a><img src={`logos/rocket-lab.png`} className="supported-operators-logo" title="Rocket Lab"/></a>
+                <a><img src={`logos/blue-origin.png`} className="supported-operators-logo" title="Blue Origin"/></a>
+                <a><img src={`logos/astra-space.png`} className="supported-operators-logo" title="Astra Space"/></a>
+                <a><img src={`logos/roscosmos.png`} className="supported-operators-logo" title="Roscosmos"/></a>
+                <a><img src={`logos/ula-white.png`} className="supported-operators-logo" title="United Launch Alliance"/></a>
+                <a><img src={`logos/virgin-orbit.png`} className="supported-operators-logo soon" title="Virgin Orbit (coming soon)"/></a>
+                <a><img src={`logos/mitsubishi-heavy-industries.png`} className="supported-operators-logo soon" title="Mitsubishi Heavy Industries (coming soon)"/></a>
 
             </div>
             
