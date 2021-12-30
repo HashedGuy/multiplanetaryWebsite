d72df969fbffab1b8248aa6c6a33911a25275968
HashedGuy <hashedguy@gmail.com>
2021-12-30 14:50:22 +0400

    add

--- src/components/Flights/Flights.js
+++ src/components/Flights/Flights.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Schedule from './Schedule'
 import './flights.css'
-import SupportedOperators from './SupportedOperators'
-import Animation from '../RocketScience/Animation'
 
 function Flights() {
     return (
